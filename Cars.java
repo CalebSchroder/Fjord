c508b8e3e2dbbfda70dcaf042b734168a7dab1b2
Caleb Schroder <doomskier@gmail.com>
2019-10-23 13:12:25 -0600

    Very close to finish

--- Cars.java
+++ Cars.java
@@ -4,7 +4,64 @@ public class Cars {
     private String name;
     private String type;
     private int liters;
-    private int hours;
+    private double hours;
+    private int type1;
+    private double mpg;
+
+
+    //constructor
+    public Cars(String name, String type, int liters, double hours) {
+        this.name = name;
+        this.type = type;
+        this.liters = liters;
+        this.hours = hours;
+        findType();
+    }
+
+    //get+set
+
+
+    //calculations
+
+    public double calculateMpg() {
+        System.out.println("Calculating mpg...");
+        mpg = calculateMpg1(liters, hours);
+        return mpg;
+
+    }
+
+    private double calculateMpg1(int liters1, double hours1) {
+        return (hours1 * 60) / (liters1 * 0.264172);
+    }
+
+    private void findType() {
+        System.out.println("Finding type");
+        if (type.equals("Sedan")) {
+            type1 = 0; 
+        }
+        if (type.equals("SUV")) {
+            type1 = 1;
+        }
+        if (type.equals("Truck")) {
+            type1 = 2;
+        }
+        type1 = 0;
+    }
+
+    public boolean standard() {
+        System.out.println("Finding if it meets the standard");
+        if (type1 == 0 && mpg >= 50) {
+            return true;
+        }
+        if (type1 == 1 && mpg >= 40) {
+            return true;            
+        }
+        if (type1 == 2 && mpg >= 30) {
+            return true;
+        } 
+            return false;
+        
+    }
 
 
 
--- Fjord.java
+++ Fjord.java
@@ -2,10 +2,65 @@ public class Fjord {
 
     public static void main(String[]args) {
         Cars car1 = new Cars("Takk", "Sedan", 50, 8);
-        Cars car2 = new Cars"Takk", "Sedan", 50, 8);
-        Cars car3 = new Cars("Takk", "Sedan", 50, 8);
-        Cars car4 = new Cars("Takk", "Sedan", 50, 8);
-        Cars car5 = new Cars("Takk", "Sedan", 50, 8);
-        Cars car6 = new Cars("Takk", "Sedan", 50, 8);
+        Cars car2 = new Cars("Beklagger", "Sedan", 40, 7.5);
+        Cars car3 = new Cars("Vakker", "SUV", 60, 5);
+        Cars car4 = new Cars("Styg", "SUV", 55, 6);
+        Cars car5 = new Cars("Vanskellig", "Truck", 65, 4.75);
+        Cars car6 = new Cars("Lastebill", "Truck", 70, 5.5);
+
+        double mpg = car1.calculateMpg();
+        boolean standard = car1.standard();
+
+        if (standard = true) {
+            System.out.println("The car meets the standards with " + mpg);
+        } else {
+            System.out.println("The car failed to meet the standards with" + mpg);
+        }
+
+        mpg = car2.calculateMpg();
+        standard = car2.standard();
+
+        if (standard = true) {
+            System.out.println("The car meets the standards with " + mpg);
+        } else {
+            System.out.println("The car failed to meet the standards with" + mpg);
+        }
+
+        mpg = car3.calculateMpg();
+        standard = car3.standard();
+
+        if (standard = true) {
+            System.out.println("The car meets the standards with " + mpg);
+        } else {
+            System.out.println("The car failed to meet the standards with" + mpg);
+        }
+        mpg = car4.calculateMpg();
+        standard = car4.standard();
+
+        if (standard = true) {
+            System.out.println("The car meets the standards with " + mpg);
+        } else {
+            System.out.println("The car failed to meet the standards with" + mpg);
+        }
+        mpg = car5.calculateMpg();
+        standard = car5.standard();
+
+        if (standard = true) {
+            System.out.println("The car meets the standards with " + mpg);
+        } else {
+            System.out.println("The car failed to meet the standards with" + mpg);
+        }
+        mpg = car6.calculateMpg();
+        standard = car6.standard();
+
+        if (standard = true) {
+            System.out.println("The car meets the standards with " + mpg);
+        } else {
+            System.out.println("The car failed to meet the standards with" + mpg);
+        }
+
+       
+
+        
     }
 }
